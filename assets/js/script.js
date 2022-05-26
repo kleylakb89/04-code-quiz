e30a8469ba544ec67006ca452296f2da5e45bd67
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-26 07:07:26 -0400

    setting up basic html structure and writing js to do list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// TODO: query states
+// TODO: initializing function
+// TODO: switch states function
+// TODO: write questions array
+// TODO: loop through questions
+// TODO: add event listeners to buttons/answers
+// TODO: write score function
+// TODO: save score and initials to local storage array of objects
+
+
+
+
+
 var timeEl= document.getElementById("time");
 
 var timeLeft = 5;
