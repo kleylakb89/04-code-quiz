0201d0eb70d115a1c87fc3897bbc598adb46985f
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-26 08:00:18 -0400

    debugged timer starting early

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,17 @@ var quizEl = document.getElementById("quiz");
 var endEl = document.getElementById("end");
 var scoresEl = document.getElementById("scores");
 var startBtn = document.getElementById("startBtn");
-var timeEl= document.getElementById("time");
+
 var testEl = document.getElementById("test");
+
 var saveBtn = document.getElementById("saveBtn");
 var againBtn = document.getElementById("againBtn");
+var timeEl = document.getElementById("time");
 
-
-// TODO: initializing function
-var init = function() {
-    switchStates();
-};
 
 
 // TODO: switch states function
-var switchStates = function() {
+var switchStates = function () {
     if (currentState === "start") {
         startEl.style.display = "block";
         quizEl.style.display = "none";
@@ -29,6 +26,7 @@ var switchStates = function() {
         quizEl.style.display = "block";
         endEl.style.display = "none";
         scoresEl.style.display = "none";
+        timer();
     } else if (currentState === "end") {
         startEl.style.display = "none";
         quizEl.style.display = "none";
@@ -42,59 +40,69 @@ var switchStates = function() {
     }
 };
 
+// TODO: initializing function
+var init = function () {
+    switchStates();
+};
 
 // TODO: add event listeners to buttons/answers
-startBtn.addEventListener("click", function(){
+startBtn.addEventListener("click", function () {
     currentState = "quiz";
     switchStates();
 });
 
-testEl.addEventListener("click", function() {
+testEl.addEventListener("click", function () {
     currentState = "end";
     switchStates();
 });
 
-saveBtn.addEventListener("click", function() {
+saveBtn.addEventListener("click", function () {
     currentState = "scores";
     switchStates();
 });
 
-againBtn.addEventListener("click", function() {
+againBtn.addEventListener("click", function () {
     currentState = "start"
     switchStates();
 });
 
+// Timer function and variable
+// TODO: Debug timer starting early
+var timeLeft = 5;
+function counter() {
+    timeEl.textContent = "Time: " + timeLeft;
+};
 
+function timer() {
+    counter();
+    var timerInterval = setInterval(function () {
+        timeLeft--;
+        counter();
+
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+        };
+    }, 1000);
+};
+
+// if (currentState === "quiz") {
+//     timer();
+// };
+
+
+// TODO: write score function
+var score = timeLeft;
+console.log(score);
 
 
 // TODO: write questions array
 // TODO: loop through questions
-// TODO: write score function
 // TODO: save score and initials to local storage array of objects
 
 
 
 
 
-// TODO: Debug timer starting early
-var timeLeft = 5;
-
-function counter() {
-    timeEl.textContent = "Time: " + timeLeft;
-}
-
-function timer() {
-    counter();
-    var timerInterval = setInterval(function() {
-        timeLeft--;
-        counter();
-
-        if (timeLeft === 0) {
-            clearInterval(timerInterval);
-        }
-    }, 1000);
-}
 
 
 init();
-timer();
