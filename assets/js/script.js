1f7c62d5be0d084283b2f76673658e4c8553e1ab
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-26 07:29:01 -0400

    added basic query states and switchStates function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,41 @@
 // TODO: query states
+var currentState = "start";
+var startEl = document.getElementById("start");
+var quizEl = document.getElementById("quiz");
+var endEl = document.getElementById("end");
+var timeEl= document.getElementById("time");
+
+
 // TODO: initializing function
+var init = function() {
+    switchStates();
+};
+
+
 // TODO: switch states function
+var switchStates = function() {
+    if (currentState === "start") {
+        startEl.style.display = "block";
+        quizEl.style.display = "none";
+        endEl.style.display = "none";
+    } else if (currentState === "quiz") {
+        startEl.style.display = "none";
+        quizEl.style.display = "block";
+        endEl.style.display = "none";
+    } else if (currentState === "end") {
+        startEl.style.display = "none";
+        quizEl.style.display = "none";
+        endEl.style.display = "block";
+    }
+};
+
+
+// TODO: add event listeners to buttons/answers
+
+
+
 // TODO: write questions array
 // TODO: loop through questions
-// TODO: add event listeners to buttons/answers
 // TODO: write score function
 // TODO: save score and initials to local storage array of objects
 
@@ -11,7 +43,6 @@
 
 
 
-var timeEl= document.getElementById("time");
 
 var timeLeft = 5;
 
@@ -31,4 +62,6 @@ function timer() {
     }, 1000);
 }
 
+
+init();
 timer();
